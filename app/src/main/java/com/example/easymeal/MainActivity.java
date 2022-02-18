70f6d03a8fc62af5407aae6e237fdb066a72d91f
Braulio <balochavezmtz99@hotmail.com>
2022-02-17 22:00:02 -0600

    Prueba

--- app/src/main/java/com/example/easymeal/MainActivity.java
+++ app/src/main/java/com/example/easymeal/MainActivity.java
@@ -18,6 +18,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        String str = "Hola";
         /*Button inicio = findViewById(R.id.inicio);
         inicio.setOnClickListener(new View.OnClickListener() {
             @Override
