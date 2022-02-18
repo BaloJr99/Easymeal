5ddd830c9a2a99fe7dd383d9b6a45415c49a4ef6
Braulio <balochavezmtz99@hotmail.com>
2022-02-17 21:57:04 -0600

    Initial commit

---
+++ app/src/main/java/com/example/easymeal/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.easymeal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    public void siguiente(View v){
+        Intent intento=new Intent(this,MainActivity2.class);
+        startActivity(intento);
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        /*Button inicio = findViewById(R.id.inicio);
+        inicio.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent (v.getContext(), MainActivity2.class);
+                startActivityForResult(intent, 0);
+    }
+});*/
+    }
+}
