c3073546d8207408a635172c20adb6adf4879d69
MiguelAngel <l18430265@piedrasnegras.tecnm.mx>
2022-02-19 16:09:01 -0600

    Base de datos con tabla simple implementada

--- app/src/main/java/com/example/easymeal/MainActivity.java
+++ app/src/main/java/com/example/easymeal/MainActivity.java
@@ -3,13 +3,17 @@ package com.example.easymeal;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
+import com.example.easymeal.database.DbAyuda;
 
 public class MainActivity extends AppCompatActivity {
 
-
+    Button btnCreate;
     public void siguiente(View v){
         Intent intento=new Intent(this,MainActivity2.class);
         startActivity(intento);
@@ -18,6 +22,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnCreate = findViewById(R.id.btnCreate);
         String str = "Hola";
         String str2 = "Hola2";
         String str3 = "Hola3";
@@ -30,5 +35,17 @@ public class MainActivity extends AppCompatActivity {
                 startActivityForResult(intent, 0);
     }
 });*/
+        btnCreate.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                DbAyuda Helper = new DbAyuda(MainActivity.this);
+                SQLiteDatabase db = Helper.getWritableDatabase();
+                if(db != null){
+                    Toast.makeText(MainActivity.this,"Base creada",Toast.LENGTH_LONG).show();
+                }else{
+                    Toast.makeText(MainActivity.this,"Base no creada",Toast.LENGTH_LONG).show();
+                }
+            }
+        });
     }
 }
