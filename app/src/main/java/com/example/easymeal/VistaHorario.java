845c5afbb26b881d19da62c67b2e7b66f4287084
mar254-tec <l18430268@piedrasnegras.tecnm.mx>
2022-04-19 11:21:58 -0500

    horario 4.0 INSERTAR DIAS FINAL

---
+++ app/src/main/java/com/example/easymeal/VistaHorario.java
@@ -0,0 +1,73 @@
+package com.example.easymeal;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class VistaHorario extends AppCompatActivity {
+    DrawerLayout dl;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vista_horario);
+        dl=findViewById(R.id.drawer_vista);
+    }
+
+    public void ClickMenu(View v){
+        //Abrimos Drawer
+        Menu.openDrawer(dl);
+    }
+
+    public void ClickLogo(View v){
+        //Cerramos drawer
+        Menu.closeDrawer(dl);
+    }
+
+    public void ClickInicio(View v){
+        //Redireccionamos activity a inicio
+        Menu.redirectActivity(this, Menu.class, "");
+    }
+
+    public void ClickRecetas(View v){
+        //Redireccionamos actividad a tablero
+        Menu.redirectActivity(this, Recetas.class, "");
+    }
+
+    public void ClickAcercaDe(View v){
+        //Recreamos actividad
+        recreate();
+    }
+
+    public void ClickHorario(View view){
+        //Redireccionamos actividad a dashboard
+        Menu.redirectActivity(this, Horario.class, "");
+    }
+
+    public void ClickLista(View view){
+        //Redireccionamos actividad a dashboard
+        Menu.redirectActivity(this, ListaMandado.class, "mandado");
+    }
+
+    public void ClickUsuarios (View v){
+        //Nos dirijimos al menu de los usuarios
+        Menu.redirectActivity(this,MenuUsuario.class, "");
+    }
+
+    public void ClickAlacena(View view) {
+        //Redireccionamos actividad a dashboard
+        Menu.redirectActivity(this, ListaMandado.class, "");
+    }
+
+    public void ClickSalir(View v){
+        //Cerramos app
+        Menu.logout(this);
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        //Cerramos Drawer
+        Menu.closeDrawer(dl);
+    }
+}
